e73bbec046c205b41123f306e6efbf2b17a1a028
spblewis <spblewis@gmail.com>
2021-06-05 11:50:39 -0500

    Wrote initial functions to return date objects for time and year

--- index.js
+++ index.js
@@ -5,5 +5,12 @@ const {
 
 const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
 
+const getYear = (number) => new Date(number, 0);
+
+/* I'm not sure about this one... needs to return date object to map times of riders.
+  Format for input is 'dd:dd', from data.Time.  Could also rewrite to pass in data.Seconds,
+  which is a number. */
+const getminutes = (string) => new Date(string);
+
 json(url)
   .then((data) => { document.getElementById('dummy').innerHTML = JSON.stringify(data); });
