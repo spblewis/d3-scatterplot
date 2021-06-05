f760d539811f952078c49d20714344e4aaf7b84d
spblewis <spblewis@gmail.com>
2021-06-05 12:01:00 -0500

    Defined SVG dimensions.  Cycled through data to generate points

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const {
   json,
+  select,
 // eslint-disable-next-line no-undef
 } = d3;
+
+const height = 500;
+const width = 800;
+const padding = 50;
+
+const svg = select('svg')
+  .attr('height', height)
+  .attr('width', width);
 
 const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
 
 const getYear = (number) => new Date(number, 0);
 
 /* I'm not sure about this one... needs to return date object to map times of riders.
-  Format for input is 'dd:dd', from data.Time.  Could also rewrite to pass in data.Seconds,
+  Format for input is 'mm:ss', from data.Time.  Could also rewrite to pass in data.Seconds,
   which is a number. */
 const getminutes = (string) => new Date(string);
 
 json(url)
-  .then((data) => { document.getElementById('dummy').innerHTML = JSON.stringify(data); });
+  .then((data) => {
+    svg.selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('cx', 100)
+      .attr('cy', 100)
+      .attr('r', '5px');
+
+    document.getElementById('dummy').innerHTML = JSON.stringify(data);
+  });
