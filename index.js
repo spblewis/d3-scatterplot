06eadcbb0fe21beb5cd3ab8f1aac9331bad53ac1
spblewis <spblewis@gmail.com>
2021-06-06 15:41:22 -0500

    Tooltips working

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const {
   axisBottom,
   axisLeft,
   scaleOrdinal,
+  event,
 // eslint-disable-next-line no-undef
 } = d3;
 
@@ -21,13 +22,15 @@ const svg = select('svg')
 
 const url = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
 
-const color = scaleOrdinal(['green', 'red']);
+const color = scaleOrdinal(['red', 'green']);
+
+const tooltip = select('body')
+  .append('div')
+  .attr('id', 'tooltip')
+  .style('display', 'none');
 
 const theYear = (number) => new Date(number, 0);
 
-/* I'm not sure about this one... needs to return date object to map times of riders.
-  Format for input is 'mm:ss', from data.Time.  Could also rewrite to pass in data.Seconds,
-  which is a number. */
 const theTime = (seconds) => new Date(seconds * 1000);
 
 json(url)
@@ -80,7 +83,18 @@ json(url)
       .attr('cx', (d) => xScale(theYear(d.Year)))
       .attr('cy', (d) => yScale(theTime(d.Seconds)))
       .attr('r', '5px')
-      .attr('fill', (d) => color(!d.Doping));
+      .attr('fill', (d) => color(!d.Doping))
+      .on('mouseover', (d) => {
+        tooltip.style('display', 'block')
+          .attr('data-year', () => theYear(d.Year))
+          .style('left', () => `${d3.event.pageX + 10}px`)
+          .style('top', () => `${d3.event.pageY - 50}px`)
+          .html(`<p>Name: ${d.Name}  Place: ${d.Place} Time: ${d.Time}</p>
+            <p>${d.Doping}</p>`);
+      })
+      .on('mouseout', () => {
+        tooltip.style('display', 'none');
+      });
 
     document.getElementById('dummy').innerHTML = JSON.stringify(data);
   });
